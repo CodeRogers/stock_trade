ec9afef4c7e652a77271f060228d22735a151bcc
pedynho <pedromourapc@gmail.com>
2021-12-01 07:17:57 -0300

    feat: goal has changed merged

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,34 +1,20 @@
 import { DateTime } from 'luxon'
 import {
   BaseModel,
-<<<<<<< Updated upstream
-  beforeSave,
-  column,
-=======
   column,
   beforeSave,
->>>>>>> Stashed changes
   HasMany,
   hasMany,
   ManyToMany,
   manyToMany,
 } from '@ioc:Adonis/Lucid/Orm'
-<<<<<<< Updated upstream
-import Stock from './Stock'
-import Document from './Document'
-=======
 import Option from './Option'
 import UserDoc from './UserDoc'
->>>>>>> Stashed changes
 import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
   public id: number
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
   @column()
   public name: string
 
@@ -41,15 +27,12 @@ export default class User extends BaseModel {
   @column({ serializeAs: null })
   public password: string
 
-<<<<<<< Updated upstream
-=======
   @beforeSave()
   public static async hashPassword(cliente: User) {
     if (cliente.$dirty.password) {
       cliente.password = await Hash.make(cliente.password)
     }
   }
->>>>>>> Stashed changes
   @column()
   public rememberMeToken?: string
 
@@ -59,21 +42,7 @@ export default class User extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
-<<<<<<< Updated upstream
-  @hasMany(() => Document)
-  public documentos: HasMany<typeof Document>
-
-  @beforeSave()
-  public static async hashPassword(auth: User) {
-    if (auth.$dirty.password) {
-      auth.password = await Hash.make(auth.password)
-    }
-  }
-
-  @manyToMany(() => Stock, {
-=======
   @manyToMany(() => Option, {
->>>>>>> Stashed changes
     localKey: 'id',
     pivotForeignKey: 'user_id',
     relatedKey: 'id',
